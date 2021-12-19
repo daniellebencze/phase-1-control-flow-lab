32ccad8319884ff75ad9c8bb2b82b16d4b8829a1
Danielle Bencze <daniellebencze@live.com>
2021-12-19 12:34:30 -0800

    commit tree b1bdb79a59cd4b5dff2abb6b89c136deb9963ae2

--- index.js
+++ index.js
@@ -1,27 +1,52 @@
-function scuberGreetingForFeet(someNumber){
-  // Write your code here!
-if (someNumber <= 400) {
-  return `This one is on me!`
-} else if (someNumber > 2500) {
-  return `No can do.`
-} else if (someNumber >= 2000) {
-  return `I will gladly take your thirty bucks.`
-}
+// function scuberGreetingForFeet(someNumber){
+//   // Write your code here!
+// if (someNumber <= 400) {
+//   return `This one is on me!`
+// } else if (someNumber > 2500) {
+//   return `No can do.`
+// } else if (someNumber >= 2000) {
+//   return `I will gladly take your thirty bucks.`
+// }
+// }
+
+// function ternaryCheckCity(city){
+//   // Write your code here!
+//   return city === "NYC" ? "Ok, sounds good." : "No go.";
+// }
+
+// function switchOnCharmFromTip (tip){
+//   // Write your code here!
+//   switch(tip){
+//   case "generous":
+//     return "Thank you so much.";
+//   case `not as generous`:
+//     return "Thank you.";
+//   default:
+//     return "Bye.";
+//   }
+// }
+
+function scuberGreetingForFeet(someValue) {
+  if (someValue <= 400) {
+    return "This one is on me!";
+  } else if (someValue > 2500) {
+    return "No can do.";
+  } else if (someValue >= 2000) {
+    return "I will gladly take your thirty bucks.";
+  }
 }
 
-function ternaryCheckCity(city){
-  // Write your code here!
+function ternaryCheckCity(city) {
   return city === "NYC" ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip (tip){
-  // Write your code here!
-  switch(tip){
-  case "generous":
-    return "Thank you so much.";
-  case `not as generous`:
-    return "Thank you.";
-  default:
-    return "Bye.";
+function switchOnCharmFromTip(tip) {
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
   }
 }
